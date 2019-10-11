027c9c9f602b2f73c3560aafc6927b1ce7407333
Ananta Dwi Prasetya Purna Yuda <anantadwi13@yahoo.com>
2019-10-12 01:05:15 +0700

    selesai

--- web_server/root/startup.sh
+++ web_server/root/startup.sh
@@ -7,10 +7,12 @@ sed -i "s/.*DB_DATABASE.*/DB_DATABASE=${DB_DATABASE}/" /var/www/pweb-reservasi/.
 sed -i "s/.*DB_USERNAME.*/DB_USERNAME=${DB_USERNAME}/" /var/www/pweb-reservasi/.env
 sed -i "s/.*DB_PASSWORD.*/DB_PASSWORD=${DB_PASSWORD}/" /var/www/pweb-reservasi/.env
 
-sleep 30
-
-mysql -h 192.168.17.30 -u haproxy_root -padmin -e "CREATE DATABASE reservasi;"
+sleep 60
 
 service apache2 restart
 
-php artisan migrate --seed
+if  [ ! -f "/var/www/initialized" ]; then
+    mysql -h 192.168.17.30 -u haproxy_root -padmin -e "CREATE DATABASE IF NOT EXISTS reservasi;"
+    php artisan migrate --seed
+    touch /var/www/initialized
+fi
