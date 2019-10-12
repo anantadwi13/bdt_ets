d1dc2be38c7913066d4246c604c509b3520f47b1
Ananta Dwi Prasetya Purna Yuda <anantadwi13@yahoo.com>
2019-10-12 13:06:27 +0700

    add failover on bootstrapping handler

--- db_server/root/startup.sh
+++ db_server/root/startup.sh
@@ -15,10 +15,6 @@ dpkg -i mysql-community-server_5.7.23-1ubuntu16.04_amd64.deb
 
 cp /root/my.cnf /etc/mysql/my.cnf
 
-if [ ${SERVER_ID} -eq 1 ]; then 
-    sed -i "s/.*loose-group_replication_bootstrap_group.*/loose-group_replication_bootstrap_group = ON/" /etc/mysql/my.cnf
-fi
-
 sed -i "s/.*loose-group_replication_group_name.*/loose-group_replication_group_name = ${GROUP_NAME}/" /etc/mysql/my.cnf
 sed -i "s/.*loose-group_replication_ip_whitelist.*/loose-group_replication_ip_whitelist = ${IP_WHITELIST}/" /etc/mysql/my.cnf
 sed -i "s/.*loose-group_replication_group_seeds.*/loose-group_replication_group_seeds = ${GROUP_SEEDS}/" /etc/mysql/my.cnf
@@ -27,11 +23,31 @@ sed -i "s/.*bind-address.*/bind-address = \"${SERVER_IP}\"/" /etc/mysql/my.cnf
 sed -i "s/.*report_host.*/report_host = \"${SERVER_IP}\"/" /etc/mysql/my.cnf
 sed -i "s/.*loose-group_replication_local_address.*/loose-group_replication_local_address = \"${SERVER_IP}:${SERVER_REPLICATION_PORT}\"/" /etc/mysql/my.cnf
 
+if [ ${SERVER_ID} -eq 1 ]; then 
+    if ! nc -z 192.168.17.32 3306 && ! nc -z 192.168.17.33 3306; then
+        sed -i "s/.*loose-group_replication_bootstrap_group.*/loose-group_replication_bootstrap_group = ON/" /etc/mysql/my.cnf
+        echo 'Starting as bootstrap'
+    fi
+fi
+
 chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
 
 rm -f /var/run/mysqld/mysqld.sock.lock
 
-service mysql restart
+if [ ${SERVER_ID} -eq 1 ]; then 
+    service mysql restart
+else
+    sleep 10
+    if [ ${SERVER_ID} -eq 2 ] &&  ! nc -z 192.168.17.31 3306 && ! nc -z 192.168.17.33 3306; then
+        sed -i "s/.*loose-group_replication_bootstrap_group.*/loose-group_replication_bootstrap_group = ON/" /etc/mysql/my.cnf
+        echo 'Starting as bootstrap'
+    fi
+    if [ ${SERVER_ID} -eq 3 ] &&  ! nc -z 192.168.17.31 3306 && ! nc -z 192.168.17.32 3306; then
+        sed -i "s/.*loose-group_replication_bootstrap_group.*/loose-group_replication_bootstrap_group = ON/" /etc/mysql/my.cnf
+        echo 'Starting as bootstrap'
+    fi
+    service mysql restart
+fi
 
 if  [ ! -f "/var/lib/mysql/initialized" ]; then
     mysql -u root -padmin < /root/cluster_init.sql
